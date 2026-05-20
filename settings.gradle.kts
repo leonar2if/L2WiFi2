5346b51c236acaccea6efcb4d3d18797c875fc87
leonar2if <Leonar20081007@gmail.com>
2026-05-20 17:27:36 -0500

    Actualizar settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.11.0"
+        id("org.jetbrains.kotlin.android") version "1.9.0"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -18,7 +22,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 rootProject.name = "L2 Wifi"
 include(":app")
  
