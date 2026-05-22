6dd406eae356a4eadd9119460e49c3628437f560
leonar2if <Leonar20081007@gmail.com>
2026-05-21 20:33:28 -0500

    Actualizar AboutActivity.kt

--- app/src/main/java/com/example/l2wifi/AboutActivity.kt
+++ app/src/main/java/com/example/l2wifi/AboutActivity.kt
@@ -5,16 +5,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class SettingsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-        findViewById<TextView>(R.id.toolbarTitle).text = "Configuración"
-        findViewById<TextView>(R.id.contentText).text = "Pantalla de configuración\n(Próximamente)"
-        findViewById<ImageView>(R.id.backButton).setOnClickListener { finish() }
-    }
-}
-
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
